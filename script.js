924e650f9d0ad9017ff248cadd645438de5d0c8e
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-04-21 14:45:13 +0530

    working on JS ES6

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-localStorage.setItem("fruit", "apple");
-// localStorage.setItem("fruit", "mango");
-let v=localStorage.getItem("fruit");
-console.log(v)
-v=localStorage.clear();
-console.log(v)
+console.log("global: ",this); 
+let a=5;
+function abc(){
+    // console.log(a); //7
+}
+function xyz(){
+    console.log("inside func: ",this); 
+    a=7;
+    abc();
+}
+xyz();
+let ob = {
+    key1: 23,
+    key2: {
+        ab2: 123,
+        ab: function () {
+            console.log("inside obj: \n", this)
+        },
+
+    }
+}
+ob.key2.ab();
