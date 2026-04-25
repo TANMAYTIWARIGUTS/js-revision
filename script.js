bb4eb9a91402e803ead50bac497a3108e720e7c7
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-04-25 11:55:17 +0530

    working

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let score = 85;
-
-if (score >= 90) {
-  console.log("Grade: A");
-} else if (score >= 80) {
-  console.log("Grade: B");
-} else {
-  console.log("Keep trying!");
+function addToCart(cb,x){
+    setTimeout(() => {
+        console.log("adding to cart ",x)
+        x++
+        cb(placeOrder, x)
+    }, 2000);
 }
+
+function createBill(cb, x){
+    setTimeout(()=>{
+        console.log("creating bill ",x)
+        x++
+        cb(x)
+    }, 500)
+}
+function placeOrder(x){
+    setTimeout(()=>{
+        console.log("placing Order ",x)
+    }, 1000)
+}
+
+// sync
+// addToCart(createBill, 1)
+
+// async
+// addToCart();
+// createBill();
+// placeOrder();
