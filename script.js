6d816c4df91581802fd0c67165e436bab5823c43
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-04-25 11:33:50 +0530

    working

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-class CreatePencils{
-    constructor(name, color, qty){
-        this.name=name;
-        this.color=color;
-        this.qty=qty;
-    }
+let score = 85;
 
-    erase(){
-        document.querySelectorAll("h1").forEach((ele)=>{
-            if(ele.style.color===this.color){
-                ele.remove();
-            }
-        })
-    }
-    write(text){
-        let h1=document.createElement("h1");
-        h1.textContent=text;
-        h1.style.color=this.color;
-        document.body.append(h1);
-    }
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else {
+  console.log("Keep trying!");
 }
-
-let pencil1=new CreatePencils("nataraj", "red",2);
-let pencil2=new CreatePencils("camlin", "yellow",5);
