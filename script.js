0826358a68e1c1910a449371b2ab048708d3fa8b
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-02 14:02:56 +0530

    async js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let pm = new Promise((res, rej) => {
-    let x = 23
-    if(x>10)
-    res(x)
-else
-    rej("not yet")
-})
+function getData(x){
+    setTimeout(()=>{
+        console.log("got data ",x)
+        return x
+    },3000)
+}
+function workData(x){
+    setTimeout(()=>{
+        console.log("working on data...");
+        return x*x;
+    },2000)
+}
+function displayData(x){
+    setTimeout(()=>{
+        console.log("got data ",x)
+    },4000)
+}
+
+async function meth(){
+    let ans = await getData(5);
+    ans= await workData(ans);
+    await displayData(ans)
+}
