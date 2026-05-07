06a942fc459ddb8866b100d25f7aadf5f8e77327
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-07 12:49:59 +0530

    working

--- script.js
+++ script.js
@@ -19,38 +19,38 @@ const getFacts = async () => {
 getFacts();
 
 
-function getData(x){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-        console.log("got data ",x)
-        res(x);
-    },3000)
-    })
-}
-function workData(x){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-        console.log("working on data...")
-        res(x*x);
-    },2000)
-    })
-}
-function displayData(x){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-        console.log("displaying data ",x)
-        res("done");
-    },1000)
-    })
-}
+// function getData(x){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//         console.log("got data ",x)
+//         res(x);
+//     },3000)
+//     })
+// }
+// function workData(x){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//         console.log("working on data...")
+//         res(x*x);
+//     },2000)
+//     })
+// }
+// function displayData(x){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//         console.log("displaying data ",x)
+//         res("done");
+//     },1000)
+//     })
+// }
 
-(async function meth(){
-    let ans = await getData(5);
-    ans= await workData(ans);
-    await displayData(ans);   
-})();
+// (async function meth(){
+//     let ans = await getData(5);
+//     ans= await workData(ans);
+//     await displayData(ans);   
+// })();
 
 
-(async function working3(){
-    console.log("hey")
-})()
+// (async function working3(){
+//     console.log("hey")
+// })()
