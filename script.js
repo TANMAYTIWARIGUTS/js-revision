3152aecfad2aea653e42cb3de951ed76645a2b11
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-10 23:48:52 +0530

    working

--- script.js
+++ script.js
@@ -54,9 +54,9 @@ getFacts();
 // (async function working3(){
 //     console.log("hey")
 // })()let count = 0;
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-    count++;
-    console.log(`Button clicked ${count} times!`);
-});
+// button.addEventListener("click", () => {
+//     count++;
+//     console.log(`Button clicked ${count} times!`);
+// });
