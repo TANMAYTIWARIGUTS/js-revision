ecc971a7de4a39d308ba5c83326a33c45b39f7f6
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-12 18:34:07 +0530

    learning

---
+++ script.js
@@ -0,0 +1,6 @@
+let inp=document.querySelector("input");
+let inp=document.querySelector("#default");
+let inp=document.querySelector("#debounce");
+let inp=document.querySelector("#throttle");
+// inp.style.backgroundColor="red";
+
