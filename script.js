29ef27061de9b6885aa408065514cdce3a0a863c
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-13 21:37:05 +0530

    js rev

--- script.js
+++ script.js
@@ -1,41 +1,6 @@
-let inp=document.querySelector("input");
-let def=document.querySelector("#default");
-let deb=document.querySelector("#debounce");
-let thr=document.querySelector("#throttle");
-// inp.style.backgroundColor="red";
-console.log(Date.now())
-
-
-const updateDebText = debounce((text)=>{
-    deb.textContent=text;
-})
-const updateThrText = throttle((text)=>{
-    thr.textContent=text;
-})
-
-inp.addEventListener("input",(e)=>{
-    def.textContent=e.target.value;
-    updateDebText(e.target.value);
-    updateThrText(e.target.value);
-})
-
-function debounce(cb, delay=1000){
-    let timeout;
-
-    return (...args)=>{
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            cb(...args);
-        }, delay);
-    }
+// 1. Reverse a string
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
-function throttle(cb,delay=1000){
-    let lastCall=0;
-    if (Date.now() - lastCall >= delay){
-return (...args)=>{
-    cb(...args);
-    }
-    }
-    
-}
+console.log(reverseString("hello"));
