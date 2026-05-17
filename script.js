1c5e9980d8f00ac85dc6c472c0e8948ab91af27c
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-17 13:41:36 +0530

    prac

--- script.js
+++ script.js
@@ -52,3 +52,9 @@ function countVowels(str) {
 }
 
 console.log(countVowels("javascript"));
+
+const arr = [1, 2, 3, 4, 5];
+
+const sum = arr.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum);
